35d872d24aa743d8914c3e153a1497c13a49b703
Run <run27017@126.com>
2020-11-17 14:05:15 +0800

    Fix bug: execute 'qall' in not vim pane

--- scripts/exit.sh
+++ scripts/exit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 terminate_vim_in_current_session() {
-    tmux list-panes -sF "#{window_id} #{pane_id} #{pane_current_command}" | grep 'vim' | awk '/[0-9]+/{ print $1 }' | while read windowId paneId; do
+    tmux list-panes -sF "#{window_id} #{pane_id} #{pane_current_command}" | grep 'vim' | awk '/[0-9]+/{ print $1 " " $2 }' | while read windowId paneId; do
         tmux select-window -t $windowId
         tmux select-pane -t $paneId
         tmux send-keys :qall
